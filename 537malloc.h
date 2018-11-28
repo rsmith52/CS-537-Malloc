8c26c94604c064e3f9c2512a0cbe61a7d322c8ef
Ray Smith <rays@royal-13.cs.wisc.edu>
2018-11-27 18:28:11 -0600

    defined tuple struct

--- 537malloc.h
+++ 537malloc.h
@@ -6,6 +6,13 @@
 #include <stdio.h>
 /* Prototypes for Variables */
 
+typedef struct Tuples {
+	int addr;
+	size_t len;
+} Tuple;
+
+/* Prototypes for Functions */
+
 void * malloc537(size_t size);
 
 void free537(void * ptr);
